03514ae1901318d8a284cdf58e8d7e55584b84ae
jokale <kalemajoanna@gmail.com>
2020-12-22 13:35:35 +0000

    render components in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Card from './Components/Card';
+import Contact from './Components/Contact';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <Card/>
+      <Contact/>
     </div>
   );
 }
