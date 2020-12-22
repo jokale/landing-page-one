f0c9aa25ef7a18b0c221c0bac22cbee392c4ab8f
jokale <kalemajoanna@gmail.com>
2020-12-22 15:17:19 +0000

    add some styling to thecard component about section

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,8 +3,10 @@ import React from 'react'
 class Card extends React.Component {
     render(){
         return(
-            <div>
-                <card> Something about the business</card>
+            <div id="about">
+                <h3 id="h3-about">About us</h3>
+                <p id="p-about"> J-Tech is one of the world leading Tech agencies. <br></br>Outsourcing the best talent across the globe for amazing clients such as Apple, Google and more</p>
+
             </div>
         )
     }
