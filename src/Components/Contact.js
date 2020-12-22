bc8935693bec33081266e00292d2fec6600df916
jokale <kalemajoanna@gmail.com>
2020-12-22 13:32:54 +0000

    build basic contact component

---
+++ src/Components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Contact extends React.Component {
+    render(){
+        return(
+            <div>
+                <footer>
+                    contact us 
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default Contact 
+
