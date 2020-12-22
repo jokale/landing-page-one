278d1aad01d5e40a685add500660d2ba2b844843
jokale <kalemajoanna@gmail.com>
2020-12-22 13:31:33 +0000

    build basic header component

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render(){
+        return(
+            <div>
+                <header>
+                    Welcome to ....
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Header 
+
