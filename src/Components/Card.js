7afaff41da4fd925d4671f5e1e621f998dac6ff4
jokale <kalemajoanna@gmail.com>
2020-12-22 13:33:55 +0000

    build basic card component

---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Card extends React.Component {
+    render(){
+        return(
+            <div>
+                <card> Something about the business</card>
+            </div>
+        )
+    }
+}
+
+export default Card 
+
