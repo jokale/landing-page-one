ca8c0c40e8fe65fd785f35442ad8fdbef45a999b
jokale <kalemajoanna@gmail.com>
2020-12-22 17:22:57 +0000

    add some styling to thecard component about section x 2

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,9 +4,26 @@ class Contact extends React.Component {
     render(){
         return(
             <div>
-                <footer>
+                <h3>
                     contact us 
-                </footer>
+                </h3>
+                <div id="contact-details">
+                    <h5>Contact us and we will get back to you in 24 hours</h5>
+                    <h5>London, United Kingdom</h5>
+                    <h5>+441234567890</h5>
+                    <h5>company@company.com</h5>
+                </div>
+                <div id="form">
+                    <form>
+                        <label>Name</label> <br></br>
+                        <input placeholder="Name"></input> <br></br>
+                        <label>Email</label> <br></br>
+                        <input placeholder="Email"></input> <br></br>
+                        <label>Attach CV</label> <br></br>
+                        <input type="file" id="myfile" name="myfile"></input> <br></br>
+                        <button>Submit</button> <br></br>
+                    </form>
+                </div>
             </div>
         )
     }
