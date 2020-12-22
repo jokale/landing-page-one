af3094c4ff8111714f674973d4cb299347908da8
jokale <kalemajoanna@gmail.com>
2020-12-22 16:40:22 +0000

    add some styling to thecard component about section and mission and vision section

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,6 +7,16 @@ class Card extends React.Component {
                 <h3 id="h3-about">About us</h3>
                 <p id="p-about"> J-Tech is one of the world leading Tech agencies. <br></br>Outsourcing the best talent across the globe for amazing clients such as Apple, Google and more</p>
 
+                 <br></br>
+            <div id="vision">
+                <h3 id="h3-vision">The vision</h3>
+                <p>Here at J-Tech we aim to ensure all our tech family are placed in <br></br> the best companies and that the best companies get the best of our talent</p>
+            </div>
+            <div id="mission">
+                <h3 id="h3-vision">The J-Tech Mission</h3>
+                <p>Here at J-Tech we aim to ensure all our tech family are placed in <br></br> the best companies and that the best companies get the best of our talent</p>
+            </div>
+
             </div>
         )
     }
