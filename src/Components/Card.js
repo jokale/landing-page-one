7554e1edf23658b1a4c3a648ba0a5039dec3cdda
jokale <kalemajoanna@gmail.com>
2020-12-28 13:24:36 +0000

    add review cards

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -23,6 +23,27 @@ class Card extends React.Component {
                 <p>Here at J-Tech we aim to ensure all our tech family are placed in <br></br> the best companies and that the best companies get the best of our talent</p>
             </div>
 
+            <div id="vision">
+                <h3 id="h3-vision">Where you fit in?</h3>
+                <p>Do you want to be working in one of the best tech companies? Then send over your CV and let us do the rest</p>
+                <p>Check out below some of our tech specialists have to say about our serivice.</p>
+                <div class="cards">
+                    <img src="/Components/download.png" alt="Avatar" />
+                     <div class="card">
+                     <h4><b>Jacob</b></h4> 
+                     <h6>Software Engineer</h6>
+                     <p>"J-Tech was ecerything I needed to step up my career" </p> <br></br>
+                    <h4><b>Helen</b></h4> 
+                     <h6>DevOpsEngineer</h6>
+                     <p>"J-Tech was ecerything I needed to step up my career" </p> <br></br>
+                     <h4><b>Samuel</b></h4> 
+                     <h6>UX/UI Engineer</h6>
+                     <p>"J-Tech was ecerything I needed to step up my career" </p> 
+                      </div>
+                   
+                </div>
+            </div>
+
             </div>
         )
     }
